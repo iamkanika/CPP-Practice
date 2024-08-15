b409bbad22aa8e4c0c362283d5748c731f263576
Kanika <kanikagarg2707@gmail.com>
2024-08-15 23:47:19 +0530

    Day 3

--- patterns.cpp
+++ patterns.cpp
@@ -38,7 +38,18 @@ void pattern4(int n){
         cout<<endl;
     }
 }
-
+void pattern7(int n){
+    for(int i=0; i<2*n-1;i++){
+        int stars=i;
+        if(i>n){
+            stars=2*n-i;
+        }
+        for(int j=1;j<stars;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
 void pattern5(int n){
     for(int i=0; i<n;i++){
         for(int j=0; j<=i;j++ ){
@@ -53,11 +64,32 @@ void pattern5(int n){
         cout<<endl;
     }
 }
+void pattern8(int n){
+    int stars=1;
+    for(int i=0; i<n;i++){
+        if (i%2==0) {
+            stars=1;}
+        else{ stars=0;}
+        for(int j=0; j<i;j++){
+        cout<<stars;
+        stars=1-stars;
+        }
+    cout<<endl;
+    }
+}
+void pattern9(int n){
+    for(int i=1; i<=n;i++){
+        for(int j=1; j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n;
     cout<<"Enter the value of n"<<endl;
     cin>>n;
-    pattern4(n);
-    pattern5(n); 
-    
+    // pattern4(n); // pattern 6
+    // pattern5(n);  //pattern6;
+pattern9(n);    
 }
